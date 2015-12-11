4c26dbfc9721251585f12955f4bed91944e11cf1
Simon Johansson <mail@simon-johansson.com>
2015-12-11 20:09:00 +0100

    Add babel presets to require hook

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 // Initiate babel for ES2015 code
-require("babel-register");
+require("babel-register")({
+  presets: ['es2015', 'stage-0']
+});
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
